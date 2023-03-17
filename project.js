5c0d9350a8d8725a474da4398729471304e1748c
Muhammad Ashhad Safwan <m_ashhadhlr@yahoo.com>
2023-03-17 07:52:27 +0500

    Added winning options & reel

--- project.js
+++ project.js
@@ -71,10 +71,74 @@ const getBet = (balance, lines) => {
 const spin = ()=> {
     const symbols = []; //The '[]' shows that the array is of fixed size, and the elemnts are yet to be added into it
     for([symbol,count] of Object.entries(SYMBOLS_COUNT)){
-
+        for(let i=0; i<count; i++){
+            symbols.push(symbol);
+        }
     }
+
+    const reels = [];
+    for (let i=0; i<COLS;i++){
+        reels.push([]);
+        const reelSymbols =[...symbols];
+        for(let j=0;j<ROWS;j++){
+            const randomIndex= Math.floor(Math.random() * reelSymbols.length);
+            const selectedSymbol = reelSymbols[randomIndex];
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex,1);
+        }
+    }
+    return reels;
+};
+
+const transpose = (reels)=>{
+    const rows =[];
+
+    for (let i=0;i<ROWS;i++){
+        rows.push([]);
+        for(let j=0; j<COLS; j++){
+            rows[i].push(reels[j][i])
+        }
+    }
+    return rows;
+};
+
+const printRows = (rows) => {
+    for (const row of rows){
+        let rowString = "";
+        for (const [i,symbol] of row.entries()){
+            rowString += symbol;
+            if (i!= row.length -1){
+                rowString += " | ";
+            }
+        }
+        console.log(rowString);
+    }
+};
+
+const getWinnings =(rows, bet, lines)=>{
+    let winnings=0;
+    for(let row =0; row<lines; row++){
+        const symbols =rows[row];
+        let allSame = true;
+
+        for (constsymbol of symbols){
+            if(symbol != symbols[0]){
+                allSame=false;
+                break;
+            }
+        }
+        if (allSame){
+            winnings += bet * SYMBOLS_VALUES[symbols[0]];
+        }
+    }
+    return winnings;
 };
 
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
+const reels=spin();
+const rows = transpose(reels);
+printRows(rows);
+const winnings= getWinnings(rows,bet,numberOfLines)
+console.log("You Won $ "+ winnings.toString()+" !!!");
